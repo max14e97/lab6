787c917b18c0f247a9a7557c24aa992b7730bf85
Hongming Zhang <hoz026@ucsd.edu>
2018-02-15 10:54:56 -0800

    ajax and facebook lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,4 +27,9 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	$.get("/project/"+idNumber, callback);
+}
+
+function callback(result) {
+	console.log(result);
 }
